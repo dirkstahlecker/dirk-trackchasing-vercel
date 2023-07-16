36d195ad3a8d54a5d4f6109bdc47f2743558ef9b
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-07-16 16:44:00 -0400

    added recaps

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ function App() {
   return (
     <div className="App box">
       <a href="/recaps/5-19-23_Clinton_County_Speedway.pdf" target="_blank">5-19-23: Clinton County Speedway</a>
+      <br/>
+      <a href="/recaps/5-20-23_Bowman_Gray_Stadium.pdf" target="_blank">5-20-23: Bowman Gray Stadium</a>
+      <br/>
+      <a href="/recaps/5-28-23_Wiscasset_Speedway.pdf" target="_blank">5-28-23: Wiscasset Speedway</a>
+      <br/>
+      <a href="/recaps/7-02-23_Chicago_Street_Course.pdf" target="_blank">7-02-23: Chicago Street Course</a>
+      <br/>
+      <a href="/recaps/7-03-23_Rockford_Speedway.pdf" target="_blank">7-03-23: Rockford Speedway</a>
+      <br/>
+      <a href="/recaps/7-05-23_Independence_Motor_Speedway.pdf" target="_blank">7-05-23: Independence Motor Speedway</a>
+      <br/>
+      <a href="/recaps/7-07-23_Farmer_City_Raceway.pdf" target="_blank">7-07-23: Farmer City Raceway</a>
+      <br/>
     </div>
   );
 }
