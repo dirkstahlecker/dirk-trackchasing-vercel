698bf00778bc5c1bc55a37fe6da8a6ec2e8c85cc
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-07-17 22:57:03 -0400

    added ed lots of recaps

--- src/App.js
+++ src/App.js
@@ -3,6 +3,42 @@ import './App.css';
 function App() {
   return (
     <div className="App box">
+      <a href="/recaps/2-01-20_Boardwalk_Hall.pdf" target="_blank">2-01-20: Boardwalk Hall</a>
+      <br/>
+      <a href="/recaps/6-19-20_Tri-State_Speedway.pdf" target="_blank">6-19-20: Tri-State Speedway</a>
+      <br/>
+      <a href="/recaps/3-26-21_Boyds_Speedway.pdf" target="_blank">3-26-21: Boyd's Speedway</a>
+      <br/>
+      <a href="/recaps/5-23-21_Central_Cycle_and_Pomfret_Speedway.pdf" target="_blank">5-23-21: Central Cycle and Pomfret Speedway</a>
+      <br/>
+      <a href="/recaps/5-29-21_Fulton_Speedway.pdf" target="_blank">5-29-21: Fulton Speedway</a>
+      <br/>
+      <a href="/recaps/2-05-22_Irwindale_Speedway.pdf" target="_blank">2-05-22: Irwindale Speedway</a>
+      <br/>
+      <a href="/recaps/2-06-22_Los_Angeles_Memorial_Coliseum.pdf" target="_blank">2-06-22: Los Angeles Memorial Colisum</a>
+      <br/>
+      <a href="/recaps/3-26-22_Cherokee_Speedway.pdf" target="_blank">3-26-22: Cherokee Speedway</a>
+      <br/>
+      <a href="/recaps/6-11-22_Ventura_Raceway.pdf" target="_blank">6-11-22: Ventura Raceway</a>
+      <br/>
+      <a href="/recaps/6-18-22_Bakersfield_Speedway_Inner_and_Kern_County_Raceway_Asphalt.pdf" target="_blank">6-18-22: Bakersfield Speedway (Inner) and Kern County Raceway (Asphalt)</a>
+      <br/>
+      <a href="/recaps/1-21-23_Circuit_de_Trois-Rivieres_Rallycross.pdf" target="_blank">1-21-23: Circuit de Trois Rivieres Rallycross</a>
+      <br/>
+      <a href="/recaps/2-09-23_Volusia_Speedway_Park.pdf" target="_blank">2-09-23: Volusia Speedway Park</a>
+      <br/>
+      <a href="/recaps/2-10-23_Auburndale_Speedway_and_Showtime_Speedway.pdf" target="_blank">2-10-23: Auburndale Speedway and Showtime Speedway</a>
+      <br/>
+      <a href="/recaps/2-11-23_Hobe_Sound_Speedway_and_Hendry_County_Motorsports_Park.pdf" target="_blank">2-11-23: Hobe Sound Speedway and Hendry County Motorsports Park</a>
+      <br/>
+      <a href="/recaps/2-12-23_New_Smyrna_Speedway.pdf" target="_blank">2-12-23: New Smyrna Speedway</a>
+      <br/>
+      <a href="/recaps/2-13-23_East_Bay_Raceway_Park.pdf" target="_blank">2-13-23: East Bay Raceway Park</a>
+      <br/>
+      <a href="/recaps/2-16-23_Citrus_County_Speedway.pdf" target="_blank">2-16-23: Citrus County Speedway</a>
+      <br/>
+      <a href="/recaps/2-16-23_Bubba_Raceway_Park.pdf" target="_blank">2-16-23: Bubba Raceway Park</a>
+      <br/>
       <a href="/recaps/5-19-23_Clinton_County_Speedway.pdf" target="_blank">5-19-23: Clinton County Speedway</a>
       <br/>
       <a href="/recaps/5-20-23_Bowman_Gray_Stadium.pdf" target="_blank">5-20-23: Bowman Gray Stadium</a>
@@ -12,6 +48,8 @@ function App() {
       <a href="/recaps/7-02-23_Chicago_Street_Course.pdf" target="_blank">7-02-23: Chicago Street Course</a>
       <br/>
       <a href="/recaps/7-03-23_Rockford_Speedway.pdf" target="_blank">7-03-23: Rockford Speedway</a>
+      <br/>
+      <a href="/recaps/7-04-23_What_Cheer_Raceway.pdf" target="_blank">7-04-23: What Cheer Raceway</a>
       <br/>
       <a href="/recaps/7-05-23_Independence_Motor_Speedway.pdf" target="_blank">7-05-23: Independence Motor Speedway</a>
       <br/>
