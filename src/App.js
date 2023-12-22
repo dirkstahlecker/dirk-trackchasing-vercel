951819a1c4788771c2558251046ef6388b2f52e7
Dirk Stahlecker <dirkstahlecker24@gmail.com>
2023-12-21 18:32:43 -0700

    added video links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <div className="App box">
       <h1 style={{color: "#FFF"}}>Dirk Trackchasing</h1>
+      <p>Visit my official Trackchasing page&nbsp;
+        <a href="http://www.roamingtheraceways.com/overall_individual.php?cid=303" 
+          target="_blank"
+          className="external-link"
+        >
+          here
+        </a>
+      </p>
       <a href="/recaps/2-01-20_Boardwalk_Hall.pdf" target="_blank">2-01-20: Boardwalk Hall</a>
       <br/>
       <a href="/recaps/6-19-20_Tri-State_Speedway.pdf" target="_blank">6-19-20: Tri-State Speedway</a>
@@ -75,6 +83,170 @@ function App() {
       <a href="/recaps/11-11-23_Podium_Club_at_Attesa.pdf" target="_blank">11-11-23: Podium Club at Attesa (Full Course)</a>
       <br/>
       <a href="/recaps/11-12-23_Shortys_Sports_Park.pdf" target="_blank">11-12-23: Shorty's Sports Park</a>
+      <br/>
+      <br/>
+      <hr/>
+      <h3>Flips</h3>
+
+      {/* <div className="flips-container">
+        <div className="flips-child flips-left">      
+        22</div>
+        <div className="flips-child flips-right">
+          <a href="https://youtu.be/a1qwU1SYpYk" target="_blank">test</a></div>
+      </div> */}
+
+      <a href="https://youtu.be/a1qwU1SYpYk" target="_blank">22 seekonk</a>
+      <br/>
+      <a href="https://youtu.be/4i868jFmovM" target="_blank">25 wall_stadium</a>
+      <br/>
+      <a href="https://youtu.be/uJ_Q3zmi5Xo" target="_blank">34 texas</a>
+      <br/>
+      <a href="https://youtu.be/4f6jP6AI6Wk" target="_blank">53 exposition center</a>
+      <br/>
+      <a href="https://youtu.be/bxHrYoygHOA" target="_blank">154_big_diamond</a>
+      <br/>
+      <a href="https://youtu.be/JlHfEv7B9oY" target="_blank">184_bridgeport</a>
+      <br/>
+      <a href="https://youtu.be/Lpuj0Ia_TIo" target="_blank">217_williams_grove</a>
+      <br/>
+      <a href="https://youtu.be/v6dxAKz2x2I" target="_blank">272_cocopah</a>
+      <br/>
+      <a href="https://youtu.be/GSBRCfsUIvw" target="_blank">283 Skagit</a>
+      <br/>
+      <a href="https://youtu.be/4kswLpMLBF4" target="_blank">298 Gas City</a>
+      <br/>
+      <a href="https://youtu.be/xoRbvtpEJzQ" target="_blank">299 Gas City</a>
+      <br/>
+      <a href="https://youtu.be/pIBlFa9WYj8" target="_blank">300 Gas City</a>
+      <br/>
+      <a href="https://youtu.be/V31CEK3JxGc" target="_blank">353 Wild Horse Pass</a>
+      <br/>
+      <a href="https://youtu.be/JxJFSBQrGfQ" target="_blank">446 Accord</a>
+      <br/>
+      <a href="https://youtu.be/aaWKd-JDPUY" target="_blank">456 Bridgeport</a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="https://youtu.be/sXHMwugWGYw" target="_blank">468 NHMS Flat Track</a>
+      <br/>
+      <a href="https://youtu.be/J0neLXCTGKQ" target="_blank">470 NHMS Flat Track</a>
+      <br/>
+      <a href="https://youtu.be/KPRf31zC8ms" target="_blank">476 Action Track USA</a>
+      <br/>
+      <a href="https://youtu.be/V_CSouGeLcY" target="_blank">477 Action Track USA</a>
+      <br/>
+      <a href="https://youtu.be/z5F29hFbHps" target="_blank">478 Lanco</a>
+      <br/>
+      <a href="https://youtu.be/afTjETpap5E" target="_blank">479 Lanco</a>
+      <br/>
+      <a href="https://youtu.be/fTnfUou_rtY" target="_blank">481 Lanco</a>
+      <br/>
+      <a href="https://youtu.be/nJX_D_TXI08" target="_blank">482 Lanco</a>
+      <br/>
+      <a href="https://youtu.be/pTmc6Hdd_Mg" target="_blank">484 Action Track</a>
+      <br/>
+      <a href="https://youtu.be/I6WAbBff5Vw" target="_blank">485 Action Track</a>
+      <br/>
+      <a href="https://youtu.be/uH7S28Vya44" target="_blank">486 Action Track</a>
+      <br/>
+      <a href="https://youtu.be/_jgSNnqIDPE" target="_blank">486 and 487 Action Track USA</a>
+      <br/>
+      <a href="https://youtu.be/UFaZP-OEo48" target="_blank">488 Path Valley Speedway Pakr</a>
+      <br/>
+      <a href="https://youtu.be/oWzRdhi2_w4" target="_blank">489 Path Valley Speedway Park</a>
+      <br/>
+      <a href="https://youtu.be/E5CMHOP_WfQ" target="_blank">490 Path Valley Speedway Park</a>
+      <br/>
+      <a href="https://youtu.be/_E7yOYQc_gg" target="_blank">492 Stockton Dirt Track</a>
+      <br/>
+      <a href="https://youtu.be/d4WoQU1iqRo" target="_blank">494 SBC Fairgrounds</a>
+      <br/>
+      <a href="https://youtu.be/TD1qBF7d7NA" target="_blank">497 SBC Fairgrounds</a>
+      <br/>
+      <a href="https://youtu.be/tcFTI27JF6w" target="_blank">498 SBC Fairgrounds</a>
+      <br/>
+      <a href="https://youtu.be/ESpuiqTLnGg" target="_blank">501 Wild Horse Pass</a>
+      <br/>
+      <a href="https://youtu.be/Mnsl-QeALUk" target="_blank">502 Wild Horse Pass</a>
+      <br/>
+      <a href="https://youtu.be/FPISEIvh8HU" target="_blank">506 and 507 Bakersfield</a>
+      <br/>
+      <a href="https://youtu.be/1DtwQHrFEZM" target="_blank">508 Placerville</a>
+      <br/>
+      <a href="https://youtu.be/au1VD2V3NOE" target="_blank">509 Placerville</a>
+      <br/>
+      <a href="https://youtu.be/ORfQndgVtSk" target="_blank">510 Placerville</a>
+      <br/>
+      <a href="https://youtu.be/xPdY1O2SHN4" target="_blank">511 Placerville</a>
+      <br/>
+      <a href="https://youtu.be/hXsyvhqvVlc" target="_blank">512 and 513 Placerville</a>
+      <br/>
+      <a href="https://youtu.be/-JdjHl4tJ-w" target="_blank">514 Placerville</a>
+      <br/>
+      <a href="https://youtu.be/ZEAJWZl_ObE" target="_blank">515 Placerville</a>
+      <br/>
+      <a href="https://youtu.be/aMpbT55AICI" target="_blank">518 and 519 Placerville</a>
+      <br/>
+      <a href="https://youtu.be/innSzTHPBQQ" target="_blank">520 Merced</a>
+      <br/>
+      <a href="https://youtu.be/btumeU4oQ_8" target="_blank">521 Merced</a>
+      <br/>
+      <a href="https://youtu.be/tdy0K-lN-vc" target="_blank">522 Merced</a>
+      <br/>
+      <a href="https://youtu.be/k2cQ8R9krnw" target="_blank">523 Merced</a>
+      <br/>
+      <a href="https://youtu.be/OnYunRldwL0" target="_blank">524_merced</a>
+      <br/>
+      <a href="https://youtu.be/s3Zdnxrn3jE" target="_blank">525_merced</a>
+      <br/>
+      <a href="https://youtu.be/D_nd52MdJZY" target="_blank">526_ventura</a>
+      <br/>
+      <a href="https://youtu.be/8LceqLfEDus" target="_blank">527_and_528_ventura</a>
+      <br/>
+      <a href="https://youtu.be/4e8pMbULF7k" target="_blank">529_ventura</a>
+      <br/>
+      <a href="https://youtu.be/_pBXfbMUoMw" target="_blank">530_and_531_ventura</a>
+      <br/>
+      <a href="https://youtu.be/JE4mZQUrLoM" target="_blank">533_gateway</a>
+      <br/>
+      <a href="https://youtu.be/iA_B2Y3ytmQ" target="_blank">534_gateway</a>
+      <br/>
+
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
+      <br/>
+      <a href="" target="_blank"></a>
       <br/>
     </div>
   );
